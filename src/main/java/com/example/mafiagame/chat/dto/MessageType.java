91eadc6a290015b0146be4aa0a692af59e4b1e49
geun <minkeon44@naver.com>
2026-01-07 13:43:00 +0900

    refactor : extract MessageType to separate file

---
+++ src/main/java/com/example/mafiagame/chat/dto/MessageType.java
@@ -0,0 +1,15 @@
+package com.example.mafiagame.chat.dto;
+
+public enum MessageType {
+    ROOM_CREATED,
+    CHAT,
+    USER_JOINED,
+    USER_LEFT,
+    HOST_CHANGED,
+    GAME_START,
+    GAME_END,
+    PHASE_CHANGE,
+    VOTE,
+    ROLE_ASSIGN,
+    GAME_RESULT
+}
