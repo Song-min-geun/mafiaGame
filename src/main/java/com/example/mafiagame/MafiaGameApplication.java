64265d5599559ee38b9e424731cfc032776adf5f
geun <minkeon44@naver.com>
2026-01-07 16:20:54 +0900

    refactor : add default roomname when roomname is null or empty

--- src/main/java/com/example/mafiagame/MafiaGameApplication.java
+++ src/main/java/com/example/mafiagame/MafiaGameApplication.java
@@ -1,12 +1,8 @@
 package com.example.mafiagame;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.example.mafiagame.user.service.UserService;
 
 @EnableScheduling
 @SpringBootApplication
@@ -14,13 +10,5 @@ public class MafiaGameApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MafiaGameApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner initDummyData(UserService userService) {
-        return args -> {
-            userService.createDummyUsers(100); // dummy1 ~ dummy100 자동 생성
-            System.out.println("✅ 테스트용 더미 유저 100명 생성 완료!");
-        };
     }
 }
