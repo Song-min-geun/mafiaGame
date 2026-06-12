a1d0f73288159018300bc013c0fd2dbbf24fee31
geun <minkeon44@naver.com>
2026-06-12 17:24:04 +0900

    ㅁㄴㄹ

--- src/main/java/com/example/mafiagame/global/config/RedisConfig.java
+++ src/main/java/com/example/mafiagame/global/config/RedisConfig.java
@@ -30,6 +30,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.SpringSessionRedisConnectionFactory;
 
 @Configuration
 @EnableCaching
@@ -91,6 +92,7 @@ public class RedisConfig {
 
     @Bean
     @Primary
+    @SpringSessionRedisConnectionFactory
     public RedisConnectionFactory supportRedisConnectionFactory() {
         RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration(supportHost, supportPort);
         standaloneConfiguration.setDatabase(supportDatabase);
